244003e6cfd3e6540d504dd44462d0988c37f1e6
KamilErtap <k.ertap32500@gmail.com>
2026-04-04 16:11:53 +0300

    Update axios base URL to remove redundant API path

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:5000/api'
-  baseURL: 'https://barista-one-api.vercel.app/api',
+  baseURL: 'https://barista-one-api.vercel.app',
 });
 
 api.interceptors.request.use((config) => {
